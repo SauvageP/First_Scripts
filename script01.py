6fc50b4f984ee3b8dd0f7eae1a7d5df7c9ea61fa
Perry <user@me.com>
2014-11-26 22:27:33 -0500

    Just created  my first python script!!

--- script01.py
+++ script01.py
@@ -1,1 +1,7 @@
+# My first Python script
 __author__ = 'Perry'
+import sys          # Load a lib mod
+print (sys.platform)
+print (2 ** 100)    # Raise 2 to a power
+x = 'Spam! '
+print (x * 8)       # String repetition
