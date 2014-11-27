7f4f6dd08a945f91e5dcbbb930f31af0e9d0e7c8
Perry <user@me.com>
2014-11-27 00:09:09 -0500

    Comments

--- threenames.py
+++ threenames.py
@@ -1,4 +1,4 @@
-# the purpose of this script is to show differences
+# The purpose of this script is to show differences
 # between Python 2 vs Python 3
 __author__ = 'Perry'
 a = 'dead'  # Define three attributes
