c52f08b75ecd2304b5cd13a91df4d0d287ad4b1c
Perry <user@me.com>
2014-11-26 23:51:53 -0500

    Comments

--- threenames.py
+++ threenames.py
@@ -1,7 +1,7 @@
 # the purpose of this script is to show differences
 # between Python 2 vs Python 3
 __author__ = 'Perry'
-a = 'dead'          # Define three attributes
+a = 'dead'  # Define three attributes
 b = 'parrot'
-c = 'sketch'        # Exported other files
-print (a,b,c)
+c = 'sketch'  # Exported other files
+print (a, b, c)
